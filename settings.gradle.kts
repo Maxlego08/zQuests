f93ad5bd7776f9b2662f4c6c95c8daf8d81423fc
Maxlego08 <maxlego08@sfr.fr>
2025-05-25 14:06:39 +0200

    :truck: rework gradle

---
+++ settings.gradle.kts
@@ -0,0 +1,13 @@
+rootProject.name = "zQuests"
+
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+include("API")
+
+file("Hooks").listFiles()?.forEach { file ->
+    if (file.isDirectory and !file.name.equals("build")) {
+        println("Include Hooks:${file.name}")
+        include(":Hooks:${file.name}")
+    }
+}
+
