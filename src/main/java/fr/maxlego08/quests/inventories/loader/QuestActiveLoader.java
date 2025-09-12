8bbde9e2d2282c328ac28a9beb336efc4a168265
Maxlego08 <maxlego08@sfr.fr>
2025-09-12 14:32:54 +0200

    :memo: Add quest active and not active button

---
+++ src/main/java/fr/maxlego08/quests/inventories/loader/QuestActiveLoader.java
@@ -0,0 +1,31 @@
+package fr.maxlego08.quests.inventories.loader;
+
+import fr.maxlego08.menu.api.button.Button;
+import fr.maxlego08.menu.api.button.DefaultButtonValue;
+import fr.maxlego08.menu.api.loader.ButtonLoader;
+import fr.maxlego08.quests.QuestsPlugin;
+import fr.maxlego08.quests.api.Quest;
+import fr.maxlego08.quests.inventories.buttons.QuestActiveButton;
+import fr.maxlego08.quests.inventories.buttons.QuestCompleteButton;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.util.List;
+import java.util.Optional;
+
+public class QuestActiveLoader extends ButtonLoader {
+
+    private final QuestsPlugin plugin;
+
+    public QuestActiveLoader(QuestsPlugin plugin) {
+        super(plugin, "ZQUESTS_ACTIVE");
+        this.plugin = plugin;
+    }
+
+    @Override
+    public Button load(YamlConfiguration configuration, String path, DefaultButtonValue defaultButtonValue) {
+
+        List<Quest> quests = configuration.getStringList(path + "quests").stream().map(questName -> this.plugin.getQuestManager().getQuest(questName)).filter(Optional::isPresent).map(Optional::get).toList();
+
+        return new QuestActiveButton(this.plugin, quests);
+    }
+}
